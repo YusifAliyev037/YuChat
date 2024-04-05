8efd17cb85eab1407b75e6a42cc766c5e62e1c09
YusifAliyev037 <131958118+YusifAliyev037@users.noreply.github.com>
2024-04-05 15:32:11 +0400

    register

--- src/Components/Pages/Register.jsx
+++ src/Components/Pages/Register.jsx
@@ -10,7 +10,7 @@ import { GoEyeClosed } from "react-icons/go";
 
 function Register() {
   const [showPassword, setShowPassword] = useState(false); 
-    const [err, setErr] = useState(false);
+  const [err, setErr] = useState(false);
   const [loading, setLoading] = useState(false);
 
   const navigate = useNavigate();
@@ -18,28 +18,29 @@ function Register() {
   const handleSubmit = async (e) => {
     setLoading(true);
     e.preventDefault();
-    const displayName = e.target[0].value.trim();
-    const email = e.target[1].value.trim();
-    const password = e.target[2].value;
-    const file = e.target[3].files[0];
+    const displayName = e.target.displayName.value.trim();
+    const email = e.target.email.value.trim();
+    const password = e.target.password.value;
+    const file = e.target.file.files[0];
 
     try {
-      //Create user
+      // Create user
       const res = await createUserWithEmailAndPassword(auth, email, password);
 
-      //Create a unique image name
+      // Create a unique image name
       const date = new Date().getTime();
       const storageRef = ref(storage, `${displayName + date}`);
 
       await uploadBytesResumable(storageRef, file).then(() => {
         getDownloadURL(storageRef).then(async (downloadURL) => {
           try {
-            //Update profile
+            // Update profile
             await updateProfile(res.user, {
               displayName,
               photoURL: downloadURL,
             });
-            //create user on firestore
+
+            // Create user on Firestore
             await setDoc(doc(db, "users", res.user.uid), {
               uid: res.user.uid,
               displayName,
@@ -48,7 +49,7 @@ function Register() {
               photoURL: downloadURL,
             });
 
-            //create empty user chats on firestore
+            // Create empty user chats on Firestore
             await setDoc(doc(db, "userChats", res.user.uid), {});
             navigate("/");
           } catch (err) {
@@ -59,42 +60,43 @@ function Register() {
         });
       });
     } catch (err) {
+      console.log(err);
       setErr(true);
       setLoading(false);
     }
   };
 
-    return (
-        <div className='formcontainer'>
-            <div className='formwrapper'>
-                <span className='logo'>Yu Chat</span>
-                <span className='title'>Register</span>
-                <form onSubmit={handleSubmit}>
-                    <input type="text" name="displayName" placeholder='Display Name' />
-                    <input type="email" name="email" placeholder='Email'/>
-                    <div className="password-input-wrapper">
-                    <input
-                      type={showPassword ? 'text' : 'password'}
-                      placeholder="Password"
-                    />
-                    <button
-                      className='passwordbutton'
-                      type="button"
-                      onClick={() => setShowPassword(!showPassword)}
-                    >
-                      {showPassword ? <GoEye color='black'  /> : <GoEyeClosed color="black" /> }  
-                   </button>
+  return (
+    <div className='formcontainer'>
+      <div className='formwrapper'>
+        <span className='logo'>Yu Chat</span>
+        <span className='title'>Register</span>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="displayName" placeholder='Display Name' />
+          <input type="email" name="email" placeholder='Email'/>
+          <div className="password-input-wrapper">
+            <input
+              type={showPassword ? 'text' : 'password'}
+              placeholder="Password"
+              name="password"
+            />
+            <button
+              className='passwordbutton'
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? <GoEye color='black'  /> : <GoEyeClosed color="black" /> }  
+            </button>
           </div>
-                  
-                    <input style={{display:"none"}} type="file" id='file' name="file" />
-                    <label htmlFor="file"><CiImageOn size={30} /> Add Profile Image</label>
-                    <button type="submit">Sign Up</button>
-                    {err && <span>Something went wrong</span>}
-                </form>
-                <p>You already have an account? <Link to="/login">Login</Link></p>
-            </div>
-        </div>
-    );
+          <input style={{display:"none"}} type="file" id='file' name="file" />
+          <label htmlFor="file"><CiImageOn size={30} /> Add Profile Image</label>
+          <button type="submit">Sign Up</button>
+          {err && <span className="error-message">Something went wrong. Please try again.</span>}
+        </form>
+        <p>You already have an account? <Link to="/login">Login</Link></p>
+      </div>
+    </div>
+  );
 }
 
 export default Register;
